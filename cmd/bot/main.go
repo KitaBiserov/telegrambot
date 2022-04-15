090d1e69bd0762c604015c5a611c30a1d373ff27
KitaBiserov <nikita-biserov@outlook.com>
2022-04-15 17:43:30 +0300

    Initial Commit

---
+++ cmd/bot/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"telegobot"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("5183055481:AAE7gMZhHWUDBFctFqIH4ip9ec83tW8lT9U")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	telegobot.NewBot()
+
+}
